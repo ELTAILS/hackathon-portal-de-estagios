765b425c0ea5152af18651ce80009a757f28cf1d
cavessadev <yuricavessa23@gmail.com>
2026-06-12 20:17:58 -0300

    Refazendo todo o VagaRepository

--- node/src/repositories/VagaRepository.ts
+++ node/src/repositories/VagaRepository.ts
@@ -3,10 +3,28 @@ import { Vaga } from "../models/Vaga";
 
 export const VagaRepository = FonteDados.getRepository(Vaga).extend({
 
-    async listarAbertas(): Promise<Vaga[]>{
-        return this.find({
-            where: {status: 'aberta'},
-            relations:['empresa']
+    async listarTodasComEmpresa(): Promise<Vaga[]>{
+        return this.find({relations: ['empresa']
         });
     },
+
+    async buscarPorIdComEmpresa(id: number): Promise<Vaga | null>{
+        return this.findOne({where: {id}, relations: ['empresa']});
+    },
+
+    async criaVaga(dados: Partial<Vaga>): Promise<Vaga>{
+        const novaVaga = this.create(dados);
+        return this.save(novaVaga);
+    },
+
+    async atualizarVaga(id: number, dados: Partial<Vaga>):Promise<Vaga | null>{
+        const vaga = await this.findOneBy({id});
+        if(!vaga) return null;
+        this.merge(vaga, dados);
+        return this.save(vaga);
+    },
+
+    async listarAbertas():Promise <Vaga[]>{
+        return this.find({where: {status: 'aberta'}, relations: ['empresa']});
+    }
 })
