f675ef61fb45ff4a055d285ac3587a671eb1551a
Wagner da silva junior <wagnerjr306@gmail.com>
2026-06-11 22:15:56 -0300

    Parte de service finalizada e mais uma função no utils

--- java/src/main/java/utils/Arquivo.java
+++ java/src/main/java/utils/Arquivo.java
@@ -1,8 +1,8 @@
 package utils;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arquivo
 {
@@ -14,4 +14,17 @@ public class Arquivo
             System.out.println(e.getMessage());
         }
     }
+
+    public static List<String> readerFile(String nomeArquivo) {
+        List<String> result = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(nomeArquivo))) {
+            reader.lines().forEach(result::add);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return result;
+    }
+
 }
