529f5ac88dee5f689c67949f66d4fb0a33ae3ce0
HiagoDz7 <levexhiago@gmail.com>
2026-06-12 12:40:56 -0300

    formas do enum alteradas, em empresa.ts e candidatura.ts

--- node/src/models/Canditadura.ts
+++ node/src/models/Canditadura.ts
@@ -2,13 +2,14 @@ import { Entity,PrimaryGeneratedColumn, Column,CreateDateColumn,ManyToOne, JoinC
 import { Aluno } from "./Aluno";
 import { Vaga } from "./Vaga";
 
+export type StatusCandidatura = 'em_analise' | 'aprovado' | 'reprovado';
 @Entity('candidaturas')
 export class Candidatura{
     @PrimaryGeneratedColumn()
     id!: number
 
     @Column({type: 'enum', enum:['em_analise', 'aprovado', 'reprovado'],default: 'em_analise'})
-    status!: string
+    status!: StatusCandidatura
     
     @ManyToOne(() => Aluno)
     @JoinColumn({name: 'aluno_id'})
