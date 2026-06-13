c03013c9b123877b874a46ad9f2a2386a2e4de1b
Wagner da silva junior <wagnerjr306@gmail.com>
2026-06-12 21:28:44 -0300

    Repo e service de empresa feita

---
+++ node/src/services/EmpresaService.ts
@@ -0,0 +1,24 @@
+import { Empresa } from "../models/Empresa";
+import { EmpresaRepository } from "../repositories/EmpresaRepository";
+
+export class AlunoService {
+    async listarTodas(): Promise<Empresa[]>{
+            return await EmpresaRepository.buscarTodos();
+        }
+    
+    async buscarPorId(id: number) : Promise <Empresa | null>{
+            return await EmpresaRepository.buscarPorId(id);
+        }
+
+    async criar(dados: Partial<Empresa>) : Promise <Empresa>{
+            return await EmpresaRepository.criaEmpresa(dados);
+        }
+    
+        async atualizar (id: number, dados: Partial<Empresa>): Promise<Empresa | null>{
+            return await EmpresaRepository.atualizarEmpresa(id,dados);     
+        }
+    
+        async remover(id: number): Promise<boolean>{
+            return await EmpresaRepository.removerEmpresa(id);
+        }
+}
