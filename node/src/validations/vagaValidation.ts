706669350475d6c85550aa936b48617fd62a1c61
HiagoDz7 <levexhiago@gmail.com>
2026-06-14 17:26:45 -0300

    Problemas com validações no zod arrumados

--- node/src/validations/vagaValidation.ts
+++ node/src/validations/vagaValidation.ts
@@ -1,16 +1,16 @@
-import z from "zod";
+import { z } from "zod";
 
-export const criarVagaSchema = z.object({ 
+export const criarVagaSchema = z.object({
     titulo: z.string().min(5, "Titulo deve ter no minimo 5 caracteres"),
     descricao: z.string().min(10, "Descricao deve ter no minimo 10 caracteres"),
     area: z.string().min(2, "Área é obrigatória"),
     status: z.enum(['aberta', 'encerrada']).optional().default('aberta'),
-    empresaId: z.number().int(),
+    empresaId: z.number({ message: "EmpresaId deve ser um numero" }).int(),
 });
 
 export const atualizarVagaSchema = z.object({
     titulo: z.string().min(5, "Titulo deve ter no minimo 5 caracteres").optional(),
     descricao: z.string().min(10, "Descricao deve ter no minimo 10 caracteres").optional(),
-    empresaId: z.number("EmpresaId deve ser um numero").int( ).optional(),
-    ativo: z.boolean().optional(),
+    empresaId: z.number({ message: "EmpresaId deve ser um numero" }).optional(),
+    area: z.string().min(2, "Área obrigatória").optional(),
 })
