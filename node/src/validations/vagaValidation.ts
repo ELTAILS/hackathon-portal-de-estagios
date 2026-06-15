95c03906a60d4c70d0418ac8a6629013bae1558e
Wagner da silva Junior <wagnerjr306@gmail.com>
2026-06-15 14:58:18 -0300

    corrigindo atualizar vaga noide

--- node/src/validations/vagaValidation.ts
+++ node/src/validations/vagaValidation.ts
@@ -9,8 +9,9 @@ export const criarVagaSchema = z.object({
 });
 
 export const atualizarVagaSchema = z.object({
-    titulo: z.string().min(5, "Titulo deve ter no minimo 5 caracteres").optional(),
-    descricao: z.string().min(10, "Descricao deve ter no minimo 10 caracteres").optional(),
-    empresaId: z.number({ message: "EmpresaId deve ser um numero" }).optional(),
-    area: z.string().min(2, "Área obrigatória").optional(),
+    titulo: z.string().min(5, "Titulo deve ter no minimo 5 caracteres"),
+    descricao: z.string().min(10, "Descricao deve ter no minimo 10 caracteres"),
+    area: z.string().min(2, "Área é obrigatória"),
+    status: z.enum(['aberta', 'encerrada']).optional().default('aberta'),
+    empresaId: z.number({ message: "EmpresaId deve ser um numero" }).int(),
 })
